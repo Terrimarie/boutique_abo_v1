c3abbf0267bcace5e1197c21c993c118bef823f0
Terri Grierson <tmgrierson@googlemail.com>
2020-11-09 11:08:51 +0000

    056 deployment part 2

--- boutique_abo/settings.py
+++ boutique_abo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#5wtfj&zr_gkmn(+-fa!t&=pr61d6kg5hj@5ryc)4_@g(ij6zm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tmg-boutique-abo.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,18 @@ WSGI_APPLICATION = 'boutique_abo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
